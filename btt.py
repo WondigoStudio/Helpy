49c2e85fb673d0ffb640ae5df040e4d27378db73
WindigoStudio <158810473+WondigoStudio@users.noreply.github.com>
2025-07-14 23:34:48 +0500

    Update btt.py

--- btt.py
+++ btt.py
@@ -154,7 +154,7 @@ async def admininfo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(commands_text, parse_mode="HTML")
 
 async def main():
-    app = ApplicationBuilder().token("AAEWyrlmCdb5xFqBvlNE8HWBtXl0n9qdpig").build()
+    app = ApplicationBuilder().token("8093659364:AAEWyrlmCdb5xFqBvlNE8HWBtXl0n9qdpig").build()
 
     app.add_handler(CommandHandler("warn", warn))
     app.add_handler(CommandHandler("mut", mut))
